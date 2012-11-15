627099b619e8817af8d97dc1211a60388e271748
grimes <>
2012-11-15 01:39:16 +0000

    Added some config files to test with

---
+++ TrackingTruthCode/SimGeneral/TrackingAnalysis/test/testStep3_HARVESTING.py
@@ -0,0 +1,72 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.392 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: harvest --datatier GEN-SIM-RAW --conditions auto:startup -s HARVESTING:validationHarvesting --eventcontent FEVTDEBUG -n -1 --filein file:val.root --fileout file:harvested.root --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HARVESTING')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.EDMtoMEAtRunEnd_cff')
+process.load('Configuration.StandardSequences.Harvesting_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    secondaryFileNames = cms.untracked.vstring(),
+    fileNames = cms.untracked.vstring('file:/afs/cern.ch/user/g/grimes/scratch0/singleMu_validation.root'),
+    processingMode = cms.untracked.string('RunsAndLumis')
+)
+
+process.options = cms.untracked.PSet(
+    Rethrow = cms.untracked.vstring('ProductNotFound'),
+    fileMode = cms.untracked.string('FULLMERGE')
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.392 $'),
+    annotation = cms.untracked.string('harvest nevts:-1'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'START60_V4::All'
+
+# Path and EndPath definitions
+process.edmtome_step = cms.Path(process.EDMtoME)
+process.validationprodHarvesting = cms.Path(process.hltpostvalidation_prod)
+process.dqmHarvestingPOGMC = cms.Path(process.DQMOffline_SecondStep_PrePOGMC)
+process.validationHarvestingFS = cms.Path(process.HarvestingFastSim)
+process.validationpreprodHarvesting = cms.Path(process.postValidation_preprod+process.hltpostvalidation_preprod)
+process.validationHarvestingHI = cms.Path(process.postValidationHI)
+process.genHarvesting = cms.Path(process.postValidation_gen)
+process.dqmHarvestingPOG = cms.Path(process.DQMOffline_SecondStep_PrePOG)
+process.alcaHarvesting = cms.Path()
+process.dqmHarvesting = cms.Path(process.DQMOffline_SecondStep+process.DQMOffline_Certification)
+process.dqmsave_step = cms.Path(process.DQMSaver)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.edmtome_step,process.validationHarvesting,process.dqmsave_step)
+
+
+process.postProcessorTrack.subDirs.append( 'Tracking/TrackNew/*' )
+process.source.fileNames = cms.untracked.vstring('file:ZEE_slimValidation.root')
+process.trackHarvesting = cms.Path(process.postProcessorTrack)
+process.schedule = cms.Schedule(process.edmtome_step,process.trackHarvesting,process.dqmsave_step)
