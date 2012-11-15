abceafeb4807abd51c94f5246061e4be4fa3d1ab
grimes <>
2012-11-15 13:21:12 +0000

    Simplified the config file and added some comments. Also put in track validation for every tracking truth collection.

--- TrackingTruthCode/SimGeneral/TrackingAnalysis/test/testStep2_DIGI_L1_DIGI2RAW_HLT_RAW2DIGI_RECO_VALIDATION_PU.py
+++ TrackingTruthCode/SimGeneral/TrackingAnalysis/test/testStep2_DIGI_L1_DIGI2RAW_HLT_RAW2DIGI_RECO_VALIDATION_PU.py
@@ -2,7 +2,7 @@
 # using: 
 # Revision: 1.392 
 # Source: /local/reps/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
-# with command line options: valTest --datatier GEN-SIM-RAW --conditions auto:startup -s DIGI,L1,DIGI2RAW,HLT,RAW2DIGI,RECO,VALIDATION --eventcontent FEVTDEBUG -n 5 --filein file:step1.root --fileout file:val.root --pileup LowLumiPileUp --pileup_input file:MinBias_60X.root --no_exec
+# with command line options: valTest --datatier GEN-SIM-RAW --conditions auto:startup -s DIGI,L1,DIGI2RAW,HLT,RAW2DIGI,RECO,VALIDATION --eventcontent FEVTDEBUG -n -1 --filein file:step1-ZEE.root --fileout file:ZEE_slimValidation.root --pileup LowLumiPileUp --pileup_input file:MinBias_60X.root --no_exec
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process('HLT')
@@ -26,13 +26,13 @@ process.load('Configuration.StandardSequences.EndOfProcess_cff')
 process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
 
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(5)
+    input = cms.untracked.int32(-1)
 )
 
 # Input source
 process.source = cms.Source("PoolSource",
     secondaryFileNames = cms.untracked.vstring(),
-    fileNames = cms.untracked.vstring('file:step1.root')
+    fileNames = cms.untracked.vstring('file:step1-ZEE.root')
 )
 
 process.options = cms.untracked.PSet(
@@ -41,7 +41,7 @@ process.options = cms.untracked.PSet(
 
 # Production Info
 process.configurationMetadata = cms.untracked.PSet(
-    version = cms.untracked.string('$Revision: 1.1 $'),
+    version = cms.untracked.string('$Revision: 1.2 $'),
     annotation = cms.untracked.string('valTest nevts:5'),
     name = cms.untracked.string('PyReleaseValidation')
 )
@@ -52,7 +52,7 @@ process.FEVTDEBUGoutput = cms.OutputModule("PoolOutputModule",
     splitLevel = cms.untracked.int32(0),
     eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
     outputCommands = process.FEVTDEBUGEventContent.outputCommands,
-    fileName = cms.untracked.string('file:val.root'),
+    fileName = cms.untracked.string('file:ZEE_slimValidation.root'),
     dataset = cms.untracked.PSet(
         filterName = cms.untracked.string(''),
         dataTier = cms.untracked.string('GEN-SIM-RAW')
@@ -90,6 +90,12 @@ from HLTrigger.Configuration.customizeHLTforMC import customizeHLTforMC
 process = customizeHLTforMC(process)
 
 # End of customisation functions
+
+###################################################################################
+##
+## Everything above is the output from cmsDriver. All of my changes are below.
+##
+###################################################################################
 
 
 #process.mix.mixObjects.mixTracks.makeCrossingFrame = False
@@ -184,10 +190,15 @@ process.mergedtruthNew = cms.EDAlias(
 process.load("SimTracker.TrackAssociation.quickTrackAssociatorByHits_cfi")
 process.quickTrackAssociatorByHits.SimToRecoDenominator = cms.string('reco')
 process.trackValidator.associators = cms.vstring('quickTrackAssociatorByHits')
-#process.trackValidator.label_tp_effic = cms.InputTag("mergedtruth")
-#process.trackValidator.label_tp_fake = cms.InputTag("mergedtruth")
-#process.trackValidator.label_tv = cms.InputTag("mergedtruth")
+# Trim off all the other track collections from validation to save time
 process.trackValidator.label = cms.VInputTag(cms.InputTag("generalTracks"))
+
+process.trackValidatorUnmerged = process.trackValidator.clone(
+	label_tp_effic=cms.InputTag("mergedtruth"),
+	label_tp_fake=cms.InputTag("mergedtruth"),
+	label_tv = cms.InputTag("mergedtruth"),
+	dirName = cms.string('Tracking/TrackUnmerged/')
+)
 
 process.trackValidatorNew = process.trackValidator.clone(
 	label_tp_effic=cms.InputTag("mergedtruthNew","MergedTrackTruth"),
@@ -196,26 +207,36 @@ process.trackValidatorNew = process.trackValidator.clone(
 	dirName = cms.string('Tracking/TrackNew/')
 )
 
+process.trackValidatorNewUnmerged = process.trackValidator.clone(
+	label_tp_effic=cms.InputTag("mergedtruthNew"),
+	label_tp_fake=cms.InputTag("mergedtruthNew"),
+	label_tv = cms.InputTag("mergedtruthNew"),
+	dirName = cms.string('Tracking/TrackNewUnmerged/')
+)
+
+# Remove these just to make sure I'm doing the same for the old and new collections
 process.prevalidation_step.remove( process.tpSelection )
 process.prevalidation_step.remove( process.tpSelecForFakeRate )
 process.prevalidation_step.remove( process.tpSelecForEfficiency )
-process.trackingTruthValid.src = cms.InputTag("mergedtruthNew","MergedTrackTruth")
+process.trackingTruthValid.src = cms.InputTag("mergedtruth","MergedTrackTruth")
 
-process.source.fileNames = cms.untracked.vstring('file:step1-ZEE.root')
-#process.source.fileNames = cms.untracked.vstring('file:singleMu_step1.root')
-process.maxEvents.input = cms.untracked.int32(-1)
 process.FEVTDEBUGoutput.outputCommands = cms.untracked.vstring( "drop *", "keep *_MEtoEDMConverter_*_*" )
-process.FEVTDEBUGoutput.fileName = cms.untracked.string('file:ZEE_slimValidation.root')
 
-process.trackingOnlyValidation = cms.Path( process.trackingTruthValid
-	+process.tracksValidation
+# Change the validation sequence to only do the tracking validation, and
+# cut out all the other stuff I'm not interested in. Also add track validation
+# for the other collections.
+process.validation = cms.Sequence(
+	process.trackingTruthValid
+	+process.trackValidator
+	+process.trackValidatorUnmerged
 	+process.trackValidatorNew
+	+process.trackValidatorNewUnmerged
 )
-process.schedule = cms.Schedule(process.digitisation_step,process.L1simulation_step,process.digi2raw_step)
-process.schedule.extend(process.HLTSchedule)
-process.schedule.extend([process.raw2digi_step,process.reconstruction_step,process.prevalidation_step,process.trackingOnlyValidation,process.endjob_step,process.FEVTDEBUGoutput_step])
 
-#process.mix.input.type = cms.string('fixed')
+## Add a moderate amount of pileup for testing
 process.mix.input.nbPileupEvents = cms.PSet( averageNumber = cms.double(20.0) )
+## Uncomment these line for simpler pileup for testing
+#process.mix.input.type = cms.string('fixed')
 #process.mix.maxBunch = cms.int32(0)
 #process.mix.minBunch = cms.int32(0)
+
