94a0509875b2e00249e086f86e36d7c5f3dcde60
grimes <>
2013-02-13 17:34:29 +0000

    First commit of the un-changed mixing module

---
+++ TrackingTruthCode/SimGeneral/MixingModule/python/mixLowLumPU_4sources_mixProdStep1_cfi.py
@@ -0,0 +1,3 @@
+import FWCore.ParameterSet.Config as cms
+
+raise RuntimeError, "Do not import obsolete file 'mixLowLumPU_4sources_mixProdStep1_cfi.py'. Two step mixing is no longer supported."
---
+++ TrackingTruthCode/SimGeneral/MixingModule/python/mixProdStep2_cfi.py
@@ -0,0 +1,2 @@
+import FWCore.ParameterSet.Config as cms
+raise RuntimeError, "Do not import obsolete file 'mixProdStep2_cfi.py'. Two step mixing is no longer supported."
