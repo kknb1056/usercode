be0fea1a11c91cb5301ddab4e6c55c9a4b8ae3dc
grimes <>
2013-03-20 17:40:04 +0000

    First commit of the new TrackingParticle implementation

--- TrackingTruthCode/SimDataFormats/TrackingAnalysis/interface/TrackingVertexContainer.h
+++ TrackingTruthCode/SimDataFormats/TrackingAnalysis/interface/TrackingVertexContainer.h
@@ -1,9 +1,12 @@
-#ifndef SimDataFormats_TrackingVertexContainer_h
-#define SimDataFormats_TrackingVertexContainer_h
-
-#include "SimDataFormats/TrackingAnalysis/interface/TrackingVertex.h"
+#ifndef SimDataFormats_TrackingAnalysis_TrackingVertexContainer_h
+#define SimDataFormats_TrackingAnalysis_TrackingVertexContainer_h
 
 #include <vector>
+#include "DataFormats/Common/interface/Ref.h"
+#include "DataFormats/Common/interface/RefVector.h"
+#include "DataFormats/Common/interface/RefProd.h"
+
+#include "SimDataFormats/TrackingAnalysis/interface/TrackingVertex.h"
 
 typedef std::vector<TrackingVertex>                TrackingVertexCollection;
 typedef edm::Ref<TrackingVertexCollection>         TrackingVertexRef;
