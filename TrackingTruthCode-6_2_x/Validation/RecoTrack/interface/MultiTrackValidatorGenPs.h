7bf8d7147da9a2d36dd7ccd74520654219641216
grimes <>
2013-04-30 17:04:46 +0000

    Re-adding files that were accidentally first commited to a branch instead of head

---
+++ TrackingTruthCode-6_2_x/Validation/RecoTrack/interface/MultiTrackValidatorGenPs.h
@@ -0,0 +1,35 @@
+#ifndef MultiTrackValidatorGenPs_h
+#define MultiTrackValidatorGenPs_h
+
+/** \class MultiTrackValidatorGenPs
+ *  Class that prodecs histrograms to validate Track Reconstruction performances
+ *
+ *  $Date: 2013/04/30 14:56:22 $
+ *  $Revision: 1.1.2.1 $
+ *  \author cerati
+ */
+
+#include "Validation/RecoTrack/interface/MultiTrackValidator.h"
+#include "SimTracker/TrackAssociation/interface/TrackAssociatorByChi2.h"
+#include "CommonTools/CandAlgos/interface/GenParticleCustomSelector.h"
+
+class MultiTrackValidatorGenPs : public MultiTrackValidator {
+ public:
+  /// Constructor
+  MultiTrackValidatorGenPs(const edm::ParameterSet& pset);
+  
+  /// Destructor
+  virtual ~MultiTrackValidatorGenPs();
+
+  /// Method called once per event
+  void analyze(const edm::Event&, const edm::EventSetup& );
+
+private:
+
+  const TrackAssociatorByChi2* associatorByChi2;
+  GenParticleCustomSelector gpSelector;				      
+
+};
+
+
+#endif
